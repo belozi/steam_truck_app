5284f03a02c619e1b175ecfa306e6382b7376db9
belozi <robert.belozi.lee@gmail.com>
2014-07-24 00:22:14 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 	before_save { email.downcase! }
+	before_create :create_remember_token
 	validates :first_name, presence: true, length: { maximum: 30 }
 	validates :last_name, presence: true, length:  { maximum: 30 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -10,4 +11,18 @@ class User < ActiveRecord::Base
 	validates :school, presence: true
 	
 	validates :password, length: { minimum: 6 }
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.digest(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+		def create_remember_token
+			self.remember_token = User.digest(User.new_remember_token)
+		end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -44,6 +44,15 @@ describe "UserPages" do
     		it "should create user" do 
     			expect { click_button submit }.to change(User, :count).by(1)
     		end
+
+            describe "after saving the user" do 
+                before      { click_button submit }
+                let(:user)  { User.find_by(email: 'user@example.com') }
+
+                it { should have_link('Sign out') }
+                it { should have_title("#{user.first_name} #{user.last_name}") }
+                it { should have_selector('div.alert.alert-success', text: "Welcome to STE(A)M Truck!") }
+            end
     	end
     end
 end
